cafad4210d95854c14b05cfc488f19e409ed4168
wa-wa <josh_flores@protonmail.com>
2018-09-26 16:19:41 -0700

    :tada: Initial Commit!

---
+++ client/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="jumbotron">
+    <h1 class="display-3">Welcome to our Community!</h1>
+    <p class="lead">A fully open source forum. Made with </p>
+    <hr class="my-4">
+    <p>This forum is all about community. Be kind! Be Helpful!</p>
+    <p class="lead">
+      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+};
+</script>
